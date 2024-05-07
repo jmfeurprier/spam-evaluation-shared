12e94cb2357f477b19d69774a957c9a59e3cecf5
Jean-Michel Feurprier <jmfeurprier@gromweb.com>
2024-05-07 17:53:32 -0400

    wip

--- src/SpamEvaluationResponse.php
+++ src/SpamEvaluationResponse.php
@@ -2,12 +2,16 @@
 
 namespace Jmf\SpamEvaluation\Shared;
 
+use Webmozart\Assert\Assert;
+
 readonly class SpamEvaluationResponse
 {
     public function __construct(
         private float $score,
         private SpamEvaluationOutcome $outcome,
     ) {
+        Assert::greaterThanEq($score, 0.0);
+        Assert::lessThanEq($score, 1.0);
     }
 
     public function getScore(): float
