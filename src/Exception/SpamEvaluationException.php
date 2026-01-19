15b7e57b72ef6444be157c3b012c7ea62cc27022
Jean-Michel Feurprier <jmfeurprier@gromweb.com>
2026-01-18 19:35:27 -0500

    Added exception

---
+++ src/Exception/SpamEvaluationException.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Jmf\SpamEvaluation\Shared\Exception;
+
+use Exception;
+
+class SpamEvaluationException extends Exception
+{
+}
--- src/SpamEvaluatorInterface.php
+++ src/SpamEvaluatorInterface.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace Jmf\SpamEvaluation\Shared;
 
+use Jmf\SpamEvaluation\Shared\Exception\SpamEvaluationException;
+
 interface SpamEvaluatorInterface
 {
+    /**
+     * @throws SpamEvaluationException
+     */
     public function evaluate(SpamEvaluationRequest $request): SpamEvaluationResponse;
 }
